cbc6cec8c59019d0338c9d283d97212c9c82735a
Sergey <Staffordrise@gmail.com>
2018-11-16 22:39:29 +0300

    Diplom remake 1.0

--- diplom/window/js/script.js
+++ diplom/window/js/script.js
@@ -4,33 +4,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
 let popup_engineer = document.querySelector('.popup_engineer'),
     overlay = document.querySelector('.overlay'),
+    headerBtn = document.querySelector('.header_btn'),
     close = document.querySelector('.popup_close');
 
-popup_engineer.addEventListener('click', function() {
+    headerBtn.addEventListener('click', function() {
     overlay.style.display = 'block';
-    this.classList.add('more-splash');
     document.body.style.overflow = 'hidden';
 });
 
 close.addEventListener('click', function() {
     overlay.style.display = 'none';
-    popup_engineer.classList.remove('more-splash');
     document.body.style.overflow = '';
 });
-
-let headerBtn = document.querySelector('.header_btn');
-
-headerBtn.addEventListener('click', function() {
-    overlay.style.display = 'block';
-    this.classList.add('more-splash');
-    document.body.style.overflow = 'hidden';
-});
-
-close.addEventListener('click', function() {
-        overlay.style.display = 'none';
-        headerBtn.classList.remove('more-splash');
-        document.body.style.overflow = '';
-    });
-    
 
 });
