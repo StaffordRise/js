b6b5f8e1d3319980d680805d7ed64ec9eb75a1a4
Sergey <Staffordrise@gmail.com>
2018-11-19 16:35:34 +0300

    + таймер

--- diplom/window/js/script.js
+++ diplom/window/js/script.js
@@ -22,7 +22,7 @@ popupEngineer.addEventListener('click', function(){
 });
 
 
-let priceBtn = document.querySelectorAll('.glazing_price_btn')
+
 
 
 // tabs
@@ -90,6 +90,64 @@ if (target && target.classList.contains('info-header-tab')) {
        }
 */
 
+/* calc
+let calc = document.querySelector('.popup_calc'),
+    priceBtn = document.querySelectorAll('.glazing_price_btn');
+*/
 
-    });
+//timer
+let deadLine = '2018-07-04';
+
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        if (hours < 10) {
+            hours = "0" + hours
+           }
+        if (minutes < 10) {
+            minutes = "0" + minutes
+           }
+        if (seconds < 10) {
+            seconds = "0" + seconds
+           };
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        }
+}
+        
+
+
+function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+     function updateClock() {
+        let t = getTimeRemaining(endtime);
+             hours.textContent = t.hours;
+             minutes.textContent = t.minutes;
+             seconds.textContent = t.seconds;
+
+        if (t.total <= 0) {
+             clearInterval(timeInterval);
+             hours.textContent = ('00');
+             minutes.textContent = ('00');
+             seconds.textContent = ('00');
+         }
+    }
+}
+setClock('timer', deadLine);
+
+
+
+});
 
